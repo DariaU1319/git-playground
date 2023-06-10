d3b38cc051eb4c90ac9ac61e14497d64523701ad
Daria Uchwat <airad15.daria@onet.pl>
2023-06-10 13:19:27 +0200

    koniec pracy nad p3.py

---
+++ pytest-example/p3.py
@@ -0,0 +1,23 @@
+def count_word_occurrence_in_file(file_name, word):
+    """
+    Counts how often word appears in file file_name.
+    Example: if file contains "one two one two three four"
+             and word is "one", then this function returns 2
+    """
+    count = 0
+    with open(file_name, 'r') as f:
+        for line in f:
+            words = line.split()
+            count += words.count(word)
+    return count
+
+import tempfile
+import os
+
+def test_count_word_occurrence_in_file():
+    _, temporary_file_name = tempfile.mkstemp()
+    with open(temporary_file_name, 'w') as f:
+        f.write("one two one two three four")
+    count = count_word_occurrence_in_file(temporary_file_name, "one")
+    assert count == 2
+    os.remove(temporary_file_name)
